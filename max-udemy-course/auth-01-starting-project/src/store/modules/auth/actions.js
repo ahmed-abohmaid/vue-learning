b5f41aba0331d9aaf77c5145ca0e22e0943af840
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2024-08-06 15:17:28 +0300

    feat: handle the login

--- max-udemy-course/auth-01-starting-project/src/store/modules/auth/actions.js
+++ max-udemy-course/auth-01-starting-project/src/store/modules/auth/actions.js
@@ -1,6 +1,41 @@
 const apiKey = process.env.VUE_APP_API_KEY;
 
 export default {
+  async login({ commit, state }, { email, password }) {
+    try {
+      state.isLoading = true;
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email,
+            password,
+            returnSecureToken: true,
+          }),
+        }
+      );
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        const error = new Error(
+          responseData.error.message || 'Failed to submit your data.'
+        );
+        throw error;
+      } else {
+        commit('setUser', {
+          token: responseData.idToken,
+          userId: responseData.localId,
+          tokenExpiration: responseData.expiresIn,
+        });
+      }
+    } catch (error) {
+      state.error = error.message;
+    } finally {
+      state.isLoading = false;
+    }
+  },
   async signup({ commit, state }, { email, password }) {
     try {
       state.isLoading = true;
@@ -20,7 +55,7 @@ export default {
 
       if (!response.ok) {
         const error = new Error(
-          responseData.error.message || 'Failed to Submit your data.'
+          responseData.error.message || 'Failed to submit your data.'
         );
         throw error;
       } else {
