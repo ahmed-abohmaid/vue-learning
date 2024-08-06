6e119806816342057327bb2ba65c5bf0c0c5a278
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2024-08-06 17:35:55 +0300

    feat: add guards for navigation

--- max-udemy-course/auth-01-starting-project/src/router.js
+++ max-udemy-course/auth-01-starting-project/src/router.js
@@ -7,6 +7,7 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
+import store from './store';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,14 +19,32 @@ const router = createRouter({
       component: CoachDetail,
       props: true,
       children: [
-        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
-      ]
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
-    { path: '/register', component: CoachRegistation },
-    { path: '/requests', component: RequestsReceived },
-    { path: '/auth', component: UserAuth },
-    { path: '/:notFound(.*)', component: NotFound }
-  ]
+    {
+      path: '/register',
+      component: CoachRegistation,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: '/auth', component: UserAuth, meta: { requiresUnAuth: true } },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnAuth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
 });
 
 export default router;
